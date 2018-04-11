cbd6a26356a4aa958e6a0d6901dd0b9c7af63577
Igor Couto <igor.fcouto@gmail.com>
2018-04-10 21:32:34 -0300

    npm package added

--- server/server.js
+++ server/server.js
@@ -14,25 +14,6 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes:
-
-app.get('/', (req, res) => {
-	res.render('index');
-});
-
-app.get('/places', (req, res) => {
-	let places;
-	res.json(places);
-});
-
-app.get('/version', (req, res) => {
-	res.write();
-});
-
-app.post('/newPlace', (req, res) => {
-	// let newPlaceSchema = new Schema(req.body);
-	// let newPlaceModel = mongoose.model('place', newPlaceSchema);
-});
 
 app.listen(door, () => {
 	console.log('server running and listen on door ' + door);
