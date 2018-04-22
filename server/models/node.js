f690f77e4d2115a19fb31d92fdf35fbbfb10b77f
Igor Couto <igor.fcouto@gmail.com>
2018-04-21 23:14:53 -0300

    Fix node model
    Add Latitude e Longitude

--- server/models/node.js
+++ server/models/node.js
@@ -2,8 +2,10 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let nodeSchema = Schema({
-	nodeID: { type: Number, required: true },
-	neighbors: [{ type : Number }]
+	nodeID: 	{ type: Number, required: true },
+	lat: 		{ type: String, required: true },
+	lng: 		{ type: String, required: true },
+	neighbors:  [{ type : Number }]
 });
 
 mongoose.model( 'Node', nodeSchema, 'nodes' );
