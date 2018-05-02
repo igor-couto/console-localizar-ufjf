2289d5dc045be2ca3f8a80fcf9bf3cc81705de02
Igor Couto <igor.fcouto@gmail.com>
2018-05-01 21:25:59 -0300

    Fix place schema

--- server/models/place.js
+++ server/models/place.js
@@ -5,7 +5,7 @@ let placeSchema = Schema({
 	name: 	{ type: String, required: true },
 	area: 	{ type: String, required: true},
 	//area: 	{ type: Schema.ObjectId, ref: 'Area' },
-	icon: {type String, required: false},
+	icon: {type : String, required: false},
 	lat: 	{ type: String, required: true },
 	lng: 	{ type: String, required: true },
 	info: 	{ type: String }
