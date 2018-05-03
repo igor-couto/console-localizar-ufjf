3b2946364b3b211296da65cbbcea485c8c4379b9
Igor Couto <igor.fcouto@gmail.com>
2018-05-03 01:00:52 -0300

    Changes in render: new model, alpha background, etc

--- server/views/js/3d-model-view.js
+++ server/views/js/3d-model-view.js
@@ -2,54 +2,63 @@ let modelContainer;
 let camera, scene, renderer;
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
+let manager = new THREE.LoadingManager();
+let model;
 init();
 animate();
 
 function init() {
 
 	modelContainer = document.getElementById('model-container');
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-	camera.position.z = 250;
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+	camera.position.x = 0;
+	camera.position.y = 0;
+	camera.position.z = -40;
 
 	// scene
 	scene = new THREE.Scene();
-	let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+	//scene.background = new THREE.Color( 0xe9e9e9 );
+	let ambientLight = new THREE.AmbientLight( 0xffffff, 1.0 );
 	scene.add( ambientLight );
 	scene.add( camera );
 
 	// texture
-	let manager = new THREE.LoadingManager();
-	manager.onProgress = function ( item, loaded, total ) {
+	manager.onProgress = ( item, loaded, total ) => {
 		console.log( item, loaded, total );
 	};
 	let textureLoader = new THREE.TextureLoader( manager );
-	let texture = textureLoader.load( 'textures/UV_Grid_Sm.jpg' );
+	let texture = textureLoader.load( 'models/hat_mario_color.png' );
 
 	// model
-	let onProgress = function ( xhr ) {
+	let onProgress = xhr => {
 		if ( xhr.lengthComputable ) {
 			let percentComplete = xhr.loaded / xhr.total * 100;
 			console.log( Math.round(percentComplete, 2) + '% downloaded' );
 		}
 	};
-	let onError = function ( xhr ) {
+	let onError = xhr => {
 	};
 	let loader = new THREE.OBJLoader( manager );
-	loader.load( 'models/obj/male02/male02.obj', function ( object ) {
+	loader.load( 'models/hat_mario_model.obj', function ( object ) {
 		object.traverse( function ( child ) {
 			if ( child instanceof THREE.Mesh ) {
 				child.material.map = texture;
 			}
 		} );
-		object.position.y = - 95;
+		object.position.x = 0;
+		object.position.y = 0;
+		object.position.z = 0;
 		scene.add( object );
+		model = object;
 	}, onProgress, onError );
 
-	renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer({ alpha: true });
 	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( 260, 120 );
+	renderer.setSize( 255, 120 );
 	modelContainer.appendChild( renderer.domElement );
 	window.addEventListener( 'resize', onWindowResize, false );
+
+	animate();
 }
 
 function onWindowResize() {
@@ -57,7 +66,7 @@ function onWindowResize() {
 	windowHalfY = window.innerHeight / 2;
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	renderer.setSize( 260 ,120 );
+	renderer.setSize( 255 ,120 );
 }
 
 function animate() {
@@ -66,6 +75,7 @@ function animate() {
 }
 
 function render() {
+	model.rotateY(0.02);
 	camera.lookAt( scene.position );
 	renderer.render( scene, camera );
 }
