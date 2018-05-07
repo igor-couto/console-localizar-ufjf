fc8b92c7c0df50269cb58d0d3f7fd5270d1faa50
Igor Couto <igor.fcouto@gmail.com>
2018-05-07 19:47:33 -0300

    Add route to upload new 3d model

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const formidable = require('formidable');
+const path = require('path');
 
 module.exports = app => {
 
@@ -13,4 +15,48 @@ module.exports = app => {
 			.then( version => res.status(201).json(version) , error => res.status(500) );
 	});
 
+	app.post('/newModel', (req, res) => {
+        let form = new formidable.IncomingForm();
+        let files = [];
+        const savePath = path.join('./','views', 'models');
+
+        let modelinfo = new Object();
+        let textures = [];
+
+        form
+            .on('file', (field, file) => {
+                files.push(file);
+            })
+            .on('end',  () => {
+                files.forEach( newfile => {
+                    let tempPath = String(newfile.path);
+                    let newpath = path.join(savePath, newfile.name);
+                    fs.rename(tempPath, newpath, err => { if (err) throw err; });
+
+                    let extension = path.extname(newfile.name);
+                    console.log(' - Extension: ' + extension);
+                    if ( extension === '.png' || extension === '.jpg') {
+                        textures.push(newfile.name);
+                        console.log(' - Texture found: ' + newfile.name);
+                    } else
+                    if ( extension === '.obj'  ) {
+                        console.log('- Obj found: '+ newfile.name);
+                        modelinfo.modelname = newfile.name; // TODO: Remove extension;
+                        modelinfo.extension = extension;
+                    }
+                    console.log('- Upload of ' + newfile.name + ' done at ' + savePath);
+                });
+
+                modelinfo.textures = textures;
+
+                let modelJSONstring= JSON.stringify(modelinfo);
+                const generatedURL = path.join('./','views', 'display', modelinfo.folder ,modelinfo.name);
+                response.render('display/display', { name : modelinfo.name, modelName : modelinfo.modelname, textures : modelinfo.textures } );
+            });
+        form.parse(req);
+	});
+
+	app.post('/newIcon', (req, res) => {
+		
+	});
 }
